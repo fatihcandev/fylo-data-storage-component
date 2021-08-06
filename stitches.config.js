b3352d7f9e27d6adb76e8463c43e009f7e819e6e
Fatih Can <fatih@90pixel.com>
2021-08-06 18:39:26 +0300

    update colors and add padding utils

--- stitches.config.js
+++ stitches.config.js
@@ -4,22 +4,20 @@ import { createCss } from '@stitches/react'
 export const { styled, getCssString, theme, global } = createCss({
   theme: {
     colors: {
-      blue: {
-        pale: 'hsl(243, 100%, 93%)',
-        grayish: 'hsl(229, 7%, 55%)',
-        dark: 'hsl(228, 56%, 26%)',
-        veryDark: 'hsl(229, 57%, 11%)',
-      },
-      gradient: {
-        primary: 'hsl(6, 100%, 80%)',
-        secondary: 'hsl(335, 100%, 65%)',
-      },
+      paleBlue: 'hsl(243, 100%, 93%)',
+      grayishBlue: 'hsl(229, 7%, 55%)',
+      darkBlue: 'hsl(228, 56%, 26%)',
+      veryDarkBlue: 'hsl(229, 57%, 11%)',
+      gradientPrimary: 'hsl(6, 100%, 80%)',
+      gradientSecondary: 'hsl(335, 100%, 65%)',
     },
   },
   media: {
     sm: '(min-width: 480px)',
     md: '(min-width: 768px)',
+    mdMax: '(max-width: 767px)',
     lg: '(min-width: 1024px)',
+    lgMax: '(max-width: 1023px)',
     xl: '(min-width: 1280px)',
   },
   utils: {
@@ -42,6 +40,26 @@ export const { styled, getCssString, theme, global } = createCss({
     my: config => value => ({
       marginTop: value,
       marginBottom: value,
+    }),
+    pt: config => value => ({
+      paddingTop: value,
+    }),
+    pr: config => value => ({
+      paddingRight: value,
+    }),
+    pb: config => value => ({
+      paddingBottom: value,
+    }),
+    pl: config => value => ({
+      paddingLeft: value,
+    }),
+    px: config => value => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: config => value => ({
+      paddingTop: value,
+      paddingBottom: value,
     }),
     size: config => value => ({
       width: value,
